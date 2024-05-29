9a83b089c2164b7692c381f84384f62f03e5c0da
Shiva Kumar Karru <103453725+Shivaksk119@users.noreply.github.com>
2024-05-30 03:09:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 //complete this code
 class Person {
 	constructor(name, age){
-		this._name = name;
-		this._age = age;
+		this.name = name;
+		this.age = age;
 	}
 
 	get name(){
-		return this._name;
+		return this.name;
 	}
 	get age(){
-		return this._age;
+		return this.age;
 	}
 
 	set name(){
-		this._name = name;
+		this.name = name;
 	}
 	set age(age){
-		this._age = age;
+		this.age = age;
 	}
 }
 
